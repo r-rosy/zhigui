72145ded7f5ced72cc55a525b9244ac76c3dc454
unknown <2313661940@qq.com>
2022-05-05 20:30:04 +0800

    rm -r

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    assetsPublicPath: './',
     proxy: {
       '/api/v1': {
           target: 'http://119.3.2.168:2333', //API服务器的地址
